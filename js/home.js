f6aa7586c48af43f3ff193ebc0e2e112288d28b8
mehadi <mehadishisir@gmail.com>
2025-09-09 17:59:40 +0600

    call the button

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-console.log("This is home.js");
+// step-1 add event listener to login button
+document.getElementById("add-money-btn").addEventListener("click", function () {
+  //   step-2 add ammount
+  const addAmmount = document.getElementById("ammount").value;
+  console.log(addAmmount);
+  // step-3 add pin
+  const pinNumber = document.getElementById("pin").value;
+  console.log(pinNumber);
+});
